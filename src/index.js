733d7a961ec183d6553d63b02666daca7ef7c7b0
Mike Hostetler <mike.hostetler@gmail.com>
2013-02-04 14:55:17 -0600

    Scaffolding Tweaks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/*
+	anvil.cordova - Anvil plugin to integrate the Cordova command line tools
+	version: 0.0.1
+	author: Mike Hostetler <mike.hostetler@gmail.com> (http://mike-hostetler.com)
+	copyright: 2012
+	license: Dual licensed 
+			 MIT (http://www.opensource.org/licenses/mit-license)
+			 GPL (http://www.opensource.org/licenses/gpl-license)
+*/
 module.exports = function( _, anvil ) {
 	require( "./cordova.scaffold")( _, anvil );
 };
